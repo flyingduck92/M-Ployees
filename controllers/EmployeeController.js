f93274dc04180441feee544f559195b13748a067
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-08-03 11:49:04 +0700

    Do getAll, add, and delete on Employee

--- controllers/EmployeeController.js
+++ controllers/EmployeeController.js
@@ -1,9 +1,59 @@
+const { Employee } = require('../models')
+
 class EmployeeController {
 
   static async getAll(req, res) {
-    res.status(200).json({
-      message: 'Hello from getAll EmployeeController'
-    })
+    try {
+      let employees = await Employee.findAll()
+
+      if (employees) {
+        res.status(200).json(employees) // success
+      } else {
+        res.status(400).json(employees) // bad request
+      }
+
+    } catch (err) {
+      res.status(500).json(err) // internal server error
+    }
+  }
+
+  static async add(req, res) {
+
+    const { name, job, age, city } = req.body
+
+    try {
+      let newEmployee = { name, job, age, city }
+      let result = await Employee.create(newEmployee)
+
+      if (result) {
+        let insertSuccess = { ...result.dataValues, message: `A new employee '${result.name}' has been added` }
+        res.status(201).json(insertSuccess) // created
+      } else {
+        res.status(400).json(result) // bad request
+      }
+    } catch (err) {
+      res.status(500).json(err) // internal server error
+    }
+
+  }
+
+
+  static async delete(req, res) {
+    let id = +req.params.id
+
+    try {
+      let result = await Employee.destroy({ where: { id } })
+
+      if (result) {
+        let deleteSuccess = { message: `An employee with an ID:'${id}' has been deleted` }
+        res.status(201).json(deleteSuccess) // created
+      } else {
+        res.status(400).json(result) // bad request
+      }
+
+    } catch (err) {
+      res.status(500).json(err) // internal server error
+    }
   }
 
 }
--- routes/employees.js
+++ routes/employees.js
@@ -4,9 +4,9 @@ const {
 
 } = require('../controllers')
 
-employeesRouter.get('/', EmployeeController.getAll) // all employees data
-// employeesRouter.get('/create',) // add brand new employee 
-// employeesRouter.get('/delete/:id',) // remove an employee 
+employeesRouter.get('/', EmployeeController.getAll)     // all employees data
+employeesRouter.post('/create', EmployeeController.add)  // add brand new employee 
+employeesRouter.delete('/delete/:id', EmployeeController.delete) // remove an employee 
 // employeesRouter.get('/update/:id',) // update an employee 
 
 
